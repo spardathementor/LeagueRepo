f6669bdd946363a7b8ab4352ddc235f4412db8ce
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-11-12 19:38:09 +0100

    MEME

---
+++ OneKeyToMeme/OneKeyToMeme/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using System.Drawing;
+using UtilityPlus.SpellTracker;
+
+namespace OneKeyToMeme
+{
+    
+    class Program
+    {
+        private static Render.Sprite sImage;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            sImage = new Render.Sprite(ImageLoader.Load("kaczorek"), new Vector2(100, 100));
+            sImage.Scale = new Vector2(100, 100);
+            sImage.Add(0);
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            sImage.OnDraw();    
+        }
+    }
+}
+    
