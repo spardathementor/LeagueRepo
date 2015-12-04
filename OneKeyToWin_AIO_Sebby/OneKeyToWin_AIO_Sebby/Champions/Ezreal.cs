62e5cb7dfc2fbfa4fc44861520237f37dcdefe32
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-12-04 11:36:59 +0100

    Ezreal Q prediction improve

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Ezreal.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Ezreal.cs
@@ -43,7 +43,7 @@ namespace OneKeyToWin_AIO_Sebby
             E = new Spell(SpellSlot.E, 475);
             R = new Spell(SpellSlot.R, 3000f);
             
-            Q.SetSkillshot(0.25f, 50f, 2000f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 60f, 2000f, true, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.25f, 80f, 1600f, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(1.2f, 160f, 2000f, false, SkillshotType.SkillshotLine);
 
