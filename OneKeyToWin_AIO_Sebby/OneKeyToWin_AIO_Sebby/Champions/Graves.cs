e86fcbafc44bc667d3802562bb7623f45dd3f651
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-12-06 18:44:45 +0100

    Graves Q prediction improve

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Graves.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Graves.cs
@@ -28,7 +28,7 @@ namespace OneKeyToWin_AIO_Sebby
             R = new Spell(SpellSlot.R, 1000f);
             R1 = new Spell(SpellSlot.R, 1500f);
 
-            Q.SetSkillshot(0.25f, 60f, 2000f, false, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 100f, 3000f, false, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.35f, 150f, 1650f, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.25f, 120f, 2100f, false, SkillshotType.SkillshotLine);
             R1.SetSkillshot(0.25f, 100f, 2100f, false, SkillshotType.SkillshotLine);
