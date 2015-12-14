75e49673fb55c7ece331762bb27dda11fa361d2c
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-12-14 20:00:29 +0100

    Tracker

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OKTWtracker.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/OKTWtracker.cs
@@ -83,7 +83,7 @@ namespace OneKeyToWin_AIO_Sebby.Core
 
         private void OnUpdate(EventArgs args)
         {
-            if (!Program.LagFree(3))
+            if (!Program.LagFree(4))
                 return;
 
             foreach (var enemy in Program.Enemies.Where(enemy => enemy.IsValid))
