51cfaeb2e27ea20386a813bebe9c57217045a47e
OneKeyToWin <kaczor.sebastian@gmail.com>
2015-12-25 17:28:55 +0100

    Kartus farm

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Karthus.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Karthus.cs
@@ -239,7 +239,7 @@ namespace OneKeyToWin_AIO_Sebby.Champions
                 {
                     foreach (var minion in allMinions.Where(minion => minion.IsValidTarget(Q.Range) && Orbwalker.InAutoAttackRange(minion)))
                     {    
-                        var hpPred = HealthPrediction.GetHealthPrediction(minion, 1000);
+                        var hpPred = HealthPrediction.GetHealthPrediction(minion, 1100);
                         if (hpPred < GetQDamage(minion) * 0.9 && hpPred > minion.Health - hpPred * 2)
                         {
                             Q.Cast(minion);
