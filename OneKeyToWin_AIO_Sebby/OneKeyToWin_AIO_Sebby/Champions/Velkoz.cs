53488f304ad19f2db7131f0a65fd14e163854fb1
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-01-12 00:27:26 +0100

    Velkoz prediction improve

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Velkoz.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Velkoz.cs
@@ -24,15 +24,15 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             Q = new Spell(SpellSlot.Q, 1180);
             QSplit = new Spell(SpellSlot.Q, 1000);
             QDummy = new Spell(SpellSlot.Q, (float)Math.Sqrt(Math.Pow(Q.Range, 2) + Math.Pow(QSplit.Range, 2)));
-            W = new Spell(SpellSlot.W, 1050);
+            W = new Spell(SpellSlot.W, 1000);
             E = new Spell(SpellSlot.E, 800);
             R = new Spell(SpellSlot.R, 1500);
 
             Q.SetSkillshot(0.25f, 70f, 1300f, true, SkillshotType.SkillshotLine);
             QSplit.SetSkillshot(0.1f, 70f, 2100f, true, SkillshotType.SkillshotLine);
-            QDummy.SetSkillshot(0.4f, 55f, 1500, false, SkillshotType.SkillshotLine);
+            QDummy.SetSkillshot(0.5f, 55f, 1500, false, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.25f, 85f, 1700f, false, SkillshotType.SkillshotLine);
-            E.SetSkillshot(0.85f, 180f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+            E.SetSkillshot(1f, 180f, float.MaxValue, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.1f, 80f, float.MaxValue, false, SkillshotType.SkillshotLine);
 
             Config.SubMenu(Player.ChampionName).SubMenu("Draw").AddItem(new MenuItem("qRange", "Q range", true).SetValue(false));
