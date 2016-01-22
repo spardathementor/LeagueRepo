799e60a4b6fb97cc058bc9547866f43108387476
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-01-22 11:04:41 +0100

    Auto lvl up notification back and humanizer improve

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/AutoLvlUp.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Core/AutoLvlUp.cs
@@ -25,11 +25,12 @@ namespace OneKeyToWin_AIO_Sebby.Core
             
             Game.OnUpdate += Game_OnGameUpdate;
             Obj_AI_Base.OnLevelUp +=Obj_AI_Base_OnLevelUp;
+            Drawing.OnDraw += Drawing_OnDraw;
         }
 
         private void Game_OnGameUpdate(EventArgs args)
         {
-            if (!Program.LagFree(0))
+            if (!Program.LagFree(0) || !Config.Item("AutoLvl").GetValue<bool>())
                 return;
             lvl1 = Config.Item("1", true).GetValue<StringList>().SelectedIndex;
             lvl2 = Config.Item("2", true).GetValue<StringList>().SelectedIndex;
@@ -43,16 +44,29 @@ namespace OneKeyToWin_AIO_Sebby.Core
                 return;
             if (lvl2 == lvl3 || lvl2 == lvl4 || lvl3 == lvl4)
                 return;
+            int delay = 700;
+            Utility.DelayAction.Add(delay, () => Up(lvl1));
+            Utility.DelayAction.Add(delay + 50, () => Up(lvl2));
+            Utility.DelayAction.Add(delay + 100, () => Up(lvl3));
+            Utility.DelayAction.Add(delay + 150, () => Up(lvl4));
+        }
 
-            Utility.DelayAction.Add(900, () => Up(lvl1));
-            Utility.DelayAction.Add(1000, () => Up(lvl2));
-            Utility.DelayAction.Add(1100, () => Up(lvl3));
-            Utility.DelayAction.Add(1200, () => Up(lvl4));
-            return;
-            Up(lvl1);
-            Up(lvl2);
-            Up(lvl3);
-            Up(lvl4);
+
+        private void Drawing_OnDraw(EventArgs args)
+        {
+            if (ObjectManager.Player.Level == 1 && Config.Item("AutoLvl").GetValue<bool>() )
+            {
+                if ((lvl2 == lvl3 || lvl2 == lvl4 || lvl3 == lvl4) && (int)Game.Time % 2 == 0)
+                {
+                    drawText("AutoLvlUp: PLEASE SET ABILITY SEQENCE", ObjectManager.Player.Position, System.Drawing.Color.OrangeRed, -200);
+                }
+            }
+        }
+
+        public static void drawText(string msg, Vector3 Hero, System.Drawing.Color color, int weight = 0)
+        {
+            var wts = Drawing.WorldToScreen(Hero);
+            Drawing.DrawText(wts[0] - (msg.Length) * 5, wts[1] + weight, color, msg);
         }
 
         private void Up(int indx)
