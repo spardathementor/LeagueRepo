029f9f4396cd460f459eda10b99a5546f880eb5b
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-02-12 18:24:44 +0100

    Genesis

--- Jinx_Genesis/Jinx_Genesis/Program.cs
+++ Jinx_Genesis/Jinx_Genesis/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
+using SebbyLib;
 
 namespace Jinx_Genesis
 {
@@ -42,7 +43,7 @@ namespace Jinx_Genesis
             if (Player.ChampionName != ChampionName) return;
 
             LoadMenu();
-
+            OktwCommon.Load();
             Q = new Spell(SpellSlot.Q, Player.AttackRange);
             W = new Spell(SpellSlot.W, 1490f);
             E = new Spell(SpellSlot.E, 900f);
