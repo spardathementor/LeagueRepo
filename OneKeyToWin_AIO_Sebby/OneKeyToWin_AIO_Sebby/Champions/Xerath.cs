fdc9d5a8d9edfeaf927ac09722c95cf61e6ddeb0
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-04-10 14:26:35 +0200

    Xerath Q prediciton fix

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Xerath.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Champions/Xerath.cs
@@ -269,8 +269,10 @@ namespace OneKeyToWin_AIO_Sebby.Champions
             {
                 if (Q.IsCharging)
                 {
-                    if (OktwCommon.GetPassiveTime(Player, "XerathArcanopulseChargeUp") < 2 || t.IsValidTarget(W.Range))
+                    if (OktwCommon.GetPassiveTime(Player, "XerathArcanopulseChargeUp") < 1 || (Player.CountEnemiesInRange(800) > 0) || Player.Distance(t) > 1450)
                         Q.Cast(Q.GetPrediction(t).CastPosition);
+                    else if(OktwCommon.GetPassiveTime(Player, "XerathArcanopulseChargeUp") < 2 || (Player.CountEnemiesInRange(1000) > 0))
+                        Q.CastIfHitchanceEquals(t, HitChance.VeryHigh);
                     else
                     {
                         Program.CastSpell(Q, t);
