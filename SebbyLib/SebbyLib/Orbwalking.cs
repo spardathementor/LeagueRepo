fdca947b63f9a1de711895eef5ef8ad9fcda3c69
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-07-25 12:30:44 +0200

    orb update

--- SebbyLib/SebbyLib/Orbwalking.cs
+++ SebbyLib/SebbyLib/Orbwalking.cs
@@ -131,9 +131,11 @@ namespace SebbyLib
                 {
                     var x = Utils.TickCount - DelayOnFire;
 
+                    if (x < 115 - Game.Ping / 2)
                     {
                         BrainFarmInt -= 2;
                     }
+                    else if (x > 135 - Game.Ping / 2)
                     {
                         BrainFarmInt += 2;
                     }
