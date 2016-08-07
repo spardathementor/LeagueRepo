471e5d63b117d3513aa40c20e89c5788cd19c1ce
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-08-07 11:41:21 +0200

    test

--- GankPlank_GENESIS/GankPlank_GENESIS/Program.cs
+++ GankPlank_GENESIS/GankPlank_GENESIS/Program.cs
@@ -95,7 +95,7 @@ namespace GankPlank_GENESIS
                 {
                     var ePos = Player.ServerPosition.Extend(Game.CursorPos, 250);
 
-                    if (!OktwCommon.CirclePoints(8, 250, ePos).Any(x => x.IsWall()) && !BarrelList.Exists(x => x.Distance(ePos) < 600))
+                    if (ePos.Distance(Player.ServerPosition) < E.Range && !OktwCommon.CirclePoints(8, 250, ePos).Any(x => x.IsWall()) && !BarrelList.Exists(x => x.Distance(ePos) < 600) )
                         E.Cast(ePos);
                 }
             }
