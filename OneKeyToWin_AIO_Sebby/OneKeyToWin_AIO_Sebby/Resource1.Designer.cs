1fc983b3fd1b60f529fd5e59a609eccefdcbc564
OneKeyToWin <kaczor.sebastian@gmail.com>
2016-12-12 20:30:07 +0100

    clean code

--- OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Resource1.Designer.cs
+++ OneKeyToWin_AIO_Sebby/OneKeyToWin_AIO_Sebby/Resource1.Designer.cs
@@ -233,9 +233,9 @@ namespace OneKeyToWin_AIO_Sebby {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Camille_Square_0_png {
+        internal static System.Drawing.Bitmap Camille_Square_0 {
             get {
-                object obj = ResourceManager.GetObject("Camille_Square_0_png", resourceCulture);
+                object obj = ResourceManager.GetObject("Camille_Square_0", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
